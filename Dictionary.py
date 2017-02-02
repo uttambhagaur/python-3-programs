4e1afed90566f0aac98f7de0a92edd37973e2abb
Uttam Singh Bhagaur <uttambhagaur@gmail.com>
2017-02-02 22:43:53 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,6 @@
+#Dictionary example
+dic = {"one":"1","two":2,"three":3}
+print(type(dic))
+print(dic["one"])
+money={"saving_account":200, "checking_account":100, "under_bed":[500,10,100]}
+print(money["under_bed"][1])
