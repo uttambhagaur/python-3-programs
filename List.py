942cadfa5af19d4b97c30a8ee55ace67f12417ab
Uttam Singh Bhagaur <uttambhagaur@gmail.com>
2017-02-02 22:30:14 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,22 @@
+list = [21,12,14,15,16,20]
+print(type(list))
+print(list[ :5])
+print(list[2:])
+print(list+[1,3,4,9])
+print(list[:])
+list[2]=120
+print(list[:])
+l=len(list)
+print(l)
+del list[5]
+list.append("add")
+print(list[:])
+#add to specific index
+list.insert(2,"position")
+print(list[:])
+#remove by content
+list.remove("position")
+print(list[:])
+#It is used to show all function of list
+dir(list)
+help(list.pop)#show how to use method/function
