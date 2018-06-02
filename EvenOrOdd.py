6a96e27e9e44f7602327ddb699c6499418b3fd8d
Uttam Singh Bhagaur <uttambhagaur@gmail.com>
2018-06-02 11:45:05 +0530

    Add files via upload

---
+++ EvenOrOdd.py
@@ -0,0 +1,9 @@
+# Program : Check number is even or odd
+# Date : 02/06/2018
+# Author : Uttam Singh Bhagaur
+
+num = int(input("Enter a number : "))
+if num%2 == 0:
+    print("Number is even")
+else :
+    print("Number is odd")
